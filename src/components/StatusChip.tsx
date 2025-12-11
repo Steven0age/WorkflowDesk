563ce95ec4819b09c0f945f739826de8fc56a24c
Stephan Haak <info@stephan-haak.com>
2025-12-11 13:17:47 +0100

    extend StatusChip.tsx: possibility to add a label and a label prefix. If no label is set, it returns status

--- src/components/StatusChip.tsx
+++ src/components/StatusChip.tsx
@@ -4,22 +4,29 @@ import theme from "../theme";
 type StatusChipTypes = {
   status: "open" | "inProgress" | "review" | "done";
   variant: "list" | "ticket";
+  label?: string;
+  labelPrefix?: string;
 };
 
-export default function StatusChip({ status, variant }: StatusChipTypes) {
-  const chipColor = {
-    open: theme.palette.status.open,
-    inProgress: theme.palette.status.inProgress,
-    review: theme.palette.status.review,
-    done: theme.palette.status.done,
-  };
-  const chipLabel = {
-    open: "Offen",
-    inProgress: "In Bearbeitung",
-    review: "Wird geprüft",
-    done: "Abgeschlossen",
-  };
+const chipColor = {
+  open: theme.palette.status.open,
+  inProgress: theme.palette.status.inProgress,
+  review: theme.palette.status.review,
+  done: theme.palette.status.done,
+};
+const chipLabel = {
+  open: "Offen",
+  inProgress: "In Bearbeitung",
+  review: "Wird geprüft",
+  done: "Abgeschlossen",
+};
 
+export default function StatusChip({
+  status,
+  variant,
+  label,
+  labelPrefix,
+}: StatusChipTypes) {
   const listStyling = {
     width: 140,
     bgcolor: chipColor[status].main,
@@ -27,16 +34,18 @@ export default function StatusChip({ status, variant }: StatusChipTypes) {
   };
 
   const ticketStyling = {
-    width: 140,
+    px: 4,
     bgcolor: chipColor[status].background,
     color: chipColor[status].contrastText,
-    border: chipColor[status].border,
+    border: `1px solid ${chipColor[status].border}`,
   };
 
   return (
     <Chip
       sx={variant === "list" ? listStyling : ticketStyling}
-      label={chipLabel[status]}
+      label={`
+        ${labelPrefix ? labelPrefix : ""} 
+        ${label ? label : chipLabel[status]}`}
     />
   );
 }
