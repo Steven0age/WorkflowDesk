7bb5578ff6ce5bfdc10913f0e7c0da23acac4eab
Stephan Haak <info@stephan-haak.com>
2025-12-15 09:19:12 +0100

    TicketModal: add footer and inset boxShadow

--- src/components/TicketModal.tsx
+++ src/components/TicketModal.tsx
@@ -27,232 +27,254 @@ export default function TicketModal({
   item,
 }: TicketModalTypes) {
   return (
-    <div>
-      <Modal
-        open={openModal}
-        onClose={handleOnClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <Modal
+      open={openModal}
+      onClose={handleOnClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          width: "1100px",
+          height: "calc(100vh - 150px)",
+          mx: "auto",
+          mt: "100px",
+          mb: "50px",
+          boxShadow: 24,
+          borderRadius: 1,
+          bgcolor: "background.paper",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: 0,
+        }}
       >
         <Box
           sx={{
-            width: "1100px",
-            height: "calc(100vh - 150px)",
-            mx: "auto",
-            mt: "100px",
-            mb: "50px",
-            boxShadow: 24,
-            p: 4,
-            borderRadius: 1,
-            bgcolor: "background.paper",
+            flex: "0 0 auto",
+            minHeight: 150,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "stretch",
+            justifyContent: "center",
+            bgcolor: theme.palette.status[item.status].main,
+            borderRadius: 10,
+            mx: 4,
+            my: 2,
+            px: 12,
+            py: 4,
+          }}
+        >
+          <Typography
+            variant="h2"
+            id="modal-modal-title"
+            sx={{
+              fontWeight: "bold",
+              fontSize: "2rem",
+              mb: 2,
+              color: theme.palette.status[item.status].contrastText,
+              textAlign: "center",
+              hyphens: "auto",
+              wordBreak: "normal",
+              overflowWrap: "break-word",
+            }}
+          >
+            Ticket: {item.label}
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              gap: 4,
+              mt: 2,
+            }}
+          >
+            <StatusChip
+              status={item.status}
+              variant="ticket"
+              labelPrefix="Status:"
+            />
+            <StatusChip
+              status={item.status}
+              variant="ticket"
+              labelPrefix="Workflow:"
+              label={item.template_title}
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            flex: "1 1 auto",
+            minHeight: 0,
             overflowY: "auto",
+            display: "grid",
+            gridTemplateColumns: "2fr 1fr",
+            gap: 4,
+            px: 4,
+            py: 2,
+            boxShadow:
+              "inset 0  8px 6px -6px #0000001f,inset 0 -8px 6px -6px #0000001f",
           }}
         >
           <Box
             sx={{
-              minHeight: 150,
               display: "flex",
               flexDirection: "column",
-              alignItems: "stretch",
-              justifyContent: "center",
-              bgcolor: theme.palette.status[item.status].main,
-              borderRadius: 10,
-              mb: 4,
-              px: 12,
-              py: 4,
+              gap: 4,
             }}
           >
-            <Typography
-              variant="h2"
-              id="modal-modal-title"
-              sx={{
-                fontWeight: "bold",
-                fontSize: "2rem",
-                mb: 2,
-                color: theme.palette.status[item.status].contrastText,
-                textAlign: "center",
-                hyphens: "auto",
-                wordBreak: "normal",
-                overflowWrap: "break-word",
-              }}
-            >
-              Ticket: {item.label}
-            </Typography>
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                gap: 4,
-                mt: 2,
-              }}
-            >
-              <StatusChip
-                status={item.status}
-                variant="ticket"
-                labelPrefix="Status:"
-              />
-              <StatusChip
-                status={item.status}
-                variant="ticket"
-                labelPrefix="Workflow:"
-                label={item.template_title}
-              />
+            <CardShell>
+              <CardHeader title="Phase: Erster Todo-Abschnitt"></CardHeader>
+              <CardContent>
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox checked disabled />}
+                    label="Schritt 1"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox checked disabled />}
+                    label="Schritt 2"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox checked disabled />}
+                    label="Schritt 3"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox checked disabled />}
+                    label="Schritt 4"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox checked disabled />}
+                    label="Schritt 5"
+                  />
+                </FormGroup>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" color="primary" disabled>
+                  Phase abgeschlossen
+                </Button>
+              </CardActions>
+            </CardShell>
+            <CardShell>
+              <CardHeader title="Phase: Zweiter Todo-Abschnitt"></CardHeader>
+              <CardContent>
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Schritt 1"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Schritt 2"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Schritt 3"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Schritt 4"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Schritt 5"
+                  />
+                </FormGroup>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" color="primary">
+                  Phase abschließen
+                </Button>
+              </CardActions>
+            </CardShell>
+            <CardShell>
+              <CardHeader title="Phase: Dritter TODO-Abschnitt"></CardHeader>
+              <CardContent>
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox disabled />}
+                    label="Schritt 1"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox disabled />}
+                    label="Schritt 2"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox disabled />}
+                    label="Schritt 3"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox disabled />}
+                    label="Schritt 4"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox disabled />}
+                    label="Schritt 5"
+                  />
+                </FormGroup>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" color="primary" disabled>
+                  Phase abschließen
+                </Button>
+              </CardActions>
+            </CardShell>
+            <Box>
+              <Typography
+                id="modal-modal-title"
+                sx={{ fontWeight: "bold", fontSize: "2rem" }}
+              >
+                Ticket: {item.label}
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Status: {item.status}
+              </Typography>
+              <Typography id="modal-modal-description">
+                Vorlagen ID: {item.template_id}
+              </Typography>
+              <Typography id="modal-modal-description">
+                Vorlagen-Name: {item.template_title}
+              </Typography>
+              <Typography id="modal-modal-description">
+                Zuständig ist: {item.assigned_to}
+              </Typography>
+              <Typography id="modal-modal-description">
+                Offen seit: {item.created_at}
+              </Typography>
+              <Typography id="modal-modal-description">
+                Erstellt von: {item.started_by}
+              </Typography>
             </Box>
           </Box>
           <Box
             sx={{
-              display: "grid",
-              gridTemplateColumns: "2fr 1fr",
-              gap: 4,
+              border: 1,
+              borderColor: "border.main",
+              borderRadius: 1,
+              p: 1,
             }}
           >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 4,
-              }}
-            >
-              <CardShell>
-                <CardHeader title="Phase: Erster Todo-Abschnitt"></CardHeader>
-              </CardShell>
-              <>
-                <CardShell>
-                  <CardHeader title="Phase: Erster Todo-Abschnitt"></CardHeader>
-                  <CardContent>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Checkbox checked disabled />}
-                        label="Schritt 1"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox checked disabled />}
-                        label="Schritt 2"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox checked disabled />}
-                        label="Schritt 3"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox checked disabled />}
-                        label="Schritt 4"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox checked disabled />}
-                        label="Schritt 5"
-                      />
-                    </FormGroup>
-                  </CardContent>
-                  <CardActions>
-                    <Button variant="contained" color="primary" disabled>
-                      Phase abgeschlossen
-                    </Button>
-                  </CardActions>
-                </CardShell>
-                <CardShell>
-                  <CardHeader title="Phase: Zweiter Todo-Abschnitt"></CardHeader>
-                  <CardContent>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Schritt 1"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Schritt 2"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Schritt 3"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Schritt 4"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked />}
-                        label="Schritt 5"
-                      />
-                    </FormGroup>
-                  </CardContent>
-                  <CardActions>
-                    <Button variant="contained" color="primary">
-                      Phase abschließen
-                    </Button>
-                  </CardActions>
-                </CardShell>
-                <CardShell>
-                  <CardHeader title="Phase: Dritter TODO-Abschnitt"></CardHeader>
-                  <CardContent>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={<Checkbox disabled />}
-                        label="Schritt 1"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox disabled />}
-                        label="Schritt 2"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox disabled />}
-                        label="Schritt 3"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox disabled />}
-                        label="Schritt 4"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox disabled />}
-                        label="Schritt 5"
-                      />
-                    </FormGroup>
-                  </CardContent>
-                  <CardActions>
-                    <Button variant="contained" color="primary" disabled>
-                      Phase abschließen
-                    </Button>
-                  </CardActions>
-                </CardShell>
-              </>
-            </Box>
-            <Box
-              sx={{
-                backgroundColor: "error.main",
-                borderRadius: 1,
-                p: 1,
-              }}
-            >
-              CONTENT-RECHTS
-            </Box>
+            CONTENT-RECHTS
           </Box>
-
-          <Typography
-            id="modal-modal-title"
-            sx={{ fontWeight: "bold", fontSize: "2rem" }}
-          >
-            Ticket: {item.label}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Status: {item.status}
-          </Typography>
-          <Typography id="modal-modal-description">
-            Vorlagen ID: {item.template_id}
-          </Typography>
-          <Typography id="modal-modal-description">
-            Vorlagen-Name: {item.template_title}
-          </Typography>
-          <Typography id="modal-modal-description">
-            Zuständig ist: {item.assigned_to}
-          </Typography>
-          <Typography id="modal-modal-description">
-            Offen seit: {item.created_at}
-          </Typography>
-          <Typography id="modal-modal-description">
-            Erstellt von: {item.started_by}
-          </Typography>
         </Box>
-      </Modal>
-    </div>
+
+        <Box
+          sx={{
+            flex: "0 0 auto",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "end",
+            alignItems: "center",
+            gap: 2,
+            pr: 2,
+          }}
+        >
+          <Button variant="contained">Speichern</Button>
+          <Button variant="outlined" onClick={handleOnClose}>
+            Schließen
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
   );
 }
