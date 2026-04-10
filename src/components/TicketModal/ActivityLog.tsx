1fbc754f4c859bc99cb067136c88b2c9e7584310
Stephan Haak <info@stephan-haak.com>
2026-04-10 08:03:32 +0200

    minor code fiexes on ActivitiyLog.tsx in order to suceed deployment (gh actions)

--- src/components/TicketModal/ActivityLog.tsx
+++ src/components/TicketModal/ActivityLog.tsx
@@ -1,33 +1,36 @@
-// import LogItem from "./LogItem";
-// import { ticketLogs } from "../../MockData/activityLogs";
-// import { Box, Typography } from "@mui/material";
-// import type { TicketDataTypes } from "../../types/types";
+//import LogItem from "./LogItem";
+import { Box, Typography } from "@mui/material";
+import type { ActivityLogDataTypes, TicketDataTypes } from "../../types/types";
 
-// type ActivitiyLogTypes = {
-//   item: TicketDataTypes;
-// };
+type ActivitiyLogTypes = {
+  item: TicketDataTypes;
+};
 
-// export default function ActivitiyLog({ item }: ActivitiyLogTypes) {
-//   return (
-//     <Box
-//       sx={{
-//         border: 1,
-//         borderColor: "border.main",
-//         borderRadius: 1,
-//         p: 2,
-//       }}
-//     >
-//       <Typography
-//         variant="h5"
-//         sx={{ pb: 2, mb: 2, borderBottom: 1, borderColor: "border.main" }}
-//       >
-//         Aktivitäten
-//       </Typography>
-//       {ticketLogs
-//         .filter((i) => i.ticket_id === item.id)
-//         .map((i) => {
-//           return <LogItem key={i.id} item={i}></LogItem>;
-//         })}
-//     </Box>
-//   );
-// }
+export default function ActivitiyLog({ item }: ActivitiyLogTypes) {
+  //const ticketLogs: ActivityLogDataTypes[] = [];
+  return (
+    <Box
+      sx={{
+        border: 1,
+        borderColor: "border.main",
+        borderRadius: 1,
+        p: 2,
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{ pb: 2, mb: 2, borderBottom: 1, borderColor: "border.main" }}
+      >
+        Aktivitäten
+      </Typography>
+      <Typography>
+        Coomin soon - Log aller Aktivitäten ist in Planung
+      </Typography>
+      {/* {ticketLogs
+        .filter((i) => i.ticket_id === item.id)
+        .map((i) => {
+          return <LogItem key={i.id} item={i}></LogItem>;
+        })} */}
+    </Box>
+  );
+}
