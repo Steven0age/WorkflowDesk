07950b97152b6ae0a80322daa73e2efdf20e39ff
Stephan Haak <info@stephan-haak.com>
2026-04-10 07:56:24 +0200

    comment out ActivityLog.tsx (is not part of the MVP)

--- src/components/TicketModal/ActivityLog.tsx
+++ src/components/TicketModal/ActivityLog.tsx
@@ -1,33 +1,33 @@
-import LogItem from "./LogItem";
-import { ticketLogs } from "../../MockData/activityLogs";
-import { Box, Typography } from "@mui/material";
-import type { TicketDataTypes } from "../../types/types";
+// import LogItem from "./LogItem";
+// import { ticketLogs } from "../../MockData/activityLogs";
+// import { Box, Typography } from "@mui/material";
+// import type { TicketDataTypes } from "../../types/types";
 
-type ActivitiyLogTypes = {
-  item: TicketDataTypes;
-};
+// type ActivitiyLogTypes = {
+//   item: TicketDataTypes;
+// };
 
-export default function ActivitiyLog({ item }: ActivitiyLogTypes) {
-  return (
-    <Box
-      sx={{
-        border: 1,
-        borderColor: "border.main",
-        borderRadius: 1,
-        p: 2,
-      }}
-    >
-      <Typography
-        variant="h5"
-        sx={{ pb: 2, mb: 2, borderBottom: 1, borderColor: "border.main" }}
-      >
-        Aktivitäten
-      </Typography>
-      {ticketLogs
-        .filter((i) => i.ticket_id === item.id)
-        .map((i) => {
-          return <LogItem key={i.id} item={i}></LogItem>;
-        })}
-    </Box>
-  );
-}
+// export default function ActivitiyLog({ item }: ActivitiyLogTypes) {
+//   return (
+//     <Box
+//       sx={{
+//         border: 1,
+//         borderColor: "border.main",
+//         borderRadius: 1,
+//         p: 2,
+//       }}
+//     >
+//       <Typography
+//         variant="h5"
+//         sx={{ pb: 2, mb: 2, borderBottom: 1, borderColor: "border.main" }}
+//       >
+//         Aktivitäten
+//       </Typography>
+//       {ticketLogs
+//         .filter((i) => i.ticket_id === item.id)
+//         .map((i) => {
+//           return <LogItem key={i.id} item={i}></LogItem>;
+//         })}
+//     </Box>
+//   );
+// }
